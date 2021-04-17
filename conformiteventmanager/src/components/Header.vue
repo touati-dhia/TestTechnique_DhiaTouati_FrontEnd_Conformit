fef7b2f0144501ce0b8ccf721a892c0344abecb7
dhia touati <touati.dhia@gmail.com>
2021-04-17 03:03:28 +0100

    shaping interface and
    shaping first interface components and adding add event button

---
+++ conformiteventmanager/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text = "Add Event" color = "green"/>
+  </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+    showAddEvent: Boolean,
+  },
+  components: {
+    Button,
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
